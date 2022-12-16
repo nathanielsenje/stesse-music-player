cda77f9a54aaf755f997a13202396fe65db031b5
nathanielsenje <topsailcashew@icloud.com>
2022-12-16 11:40:49 +0300

    changed logo and favicon

--- src/componenents/Navigation.js
+++ src/componenents/Navigation.js
@@ -1,12 +1,13 @@
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form'
+import Image from 'react-bootstrap/Image'
 
 function Navigation() {
   return (
     <Nav justify defaultActiveKey="/home">
       <Nav.Item>
         <Nav.Link href="/home" className='logo'>
-          <h2>Stesse</h2>
+          {/* <Image src="/public/logo/PNG/logo3.png"></Image> */}
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
