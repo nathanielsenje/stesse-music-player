4a10949294829675a6b64f48234fddb8c696e00c
nathanielsenje <topsailcashew@icloud.com>
2022-12-17 20:27:46 +0300

    Added seek animation

--- src/componenents/Seeker.js
+++ src/componenents/Seeker.js
@@ -1,10 +1,10 @@
 import { Container, Row, Col, ProgressBar } from 'react-bootstrap';
 
 function Seeker() {
-  const now = 60;
+  const now = 100;
   return (
     <Container className='mt-3 text-center'>
-      <h4>4:23</h4><ProgressBar className='seek' now={now} />
+      <h4>3:12</h4><ProgressBar className='seek' now={now} />
     </Container>
     )
 }
